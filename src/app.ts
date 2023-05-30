ff970202a1c21d5974174818d748bb1cbc3a5afa
akilinjamam <akilinjamam@gmail.comm>
2023-05-30 16:09:27 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request } from 'express'
 import cors from 'cors'
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
+
+// parser
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
 })
-
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 const connector = async () => {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database is connected successfully');
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected successfully')
 
-        app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-        })
-    } catch (error) {
-        console.log(`failed to load data ${error}`)
-    }
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`failed to load data ${error}`)
+  }
 }
 
-connector();
-
+connector()
